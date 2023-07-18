053d728c997a178be74434b60349a25a801a905b
Bulat Sharifullin <good1hare@gmail.com>
2023-07-18 17:24:23 +0300

    add commands

---
+++ internal/controller/telegramHandler/noteCommand.go
@@ -0,0 +1,14 @@
+package telegramHandler
+
+import telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+func (th *TelegramHandler) NoteCommand() {
+	msg := telegram.NewMessage(th.update.Message.Chat.ID, "")
+	msg.ReplyToMessageID = th.update.Message.MessageID
+
+	msg.Text = "Чем могу вам помочь?"
+	_, err := th.bot.Send(msg)
+	if err != nil {
+		return
+	}
+}
--- internal/controller/telegramHandler/telegramHandler.go
+++ internal/controller/telegramHandler/telegramHandler.go
@@ -29,27 +29,30 @@ func (th *TelegramHandler) Handle() {
 
 	th.register()
 
-	if !th.update.Message.IsCommand() { // ignore any non-command Messages
-		return
+	//if !th.update.Message.IsCommand() { // ignore any non-command Messages
+	//	return
+	//}
+
+	command := th.update.Message.Text
+
+	if th.update.Message.IsCommand() && th.update.Message.Command() == "start" {
+		th.StartCommand()
 	}
 
-	command := th.update.Message.Command()
 	switch command {
-	case "start":
-		th.HelloAnswer()
-	case "help":
-		th.HelpAnswer()
+	case "Быстрая заметка":
+		th.StartCommand()
+	case "Заметка":
+		th.NoteCommand()
+	case "Напоминание":
+		th.ReminderCommand()
+	case "Помощь":
+		th.HelpCommand()
+	case "Разработчику на кофе":
+		th.DonationCommand()
 	default:
 		th.DefaultAnswer()
 	}
-
-	//msg := telegram.NewMessage(th.update.Message.Chat.ID, th.update.Message.Text)
-	//msg.ReplyToMessageID = th.update.Message.MessageID
-	//
-	//_, err := th.bot.Send(msg)
-	//if err != nil {
-	//	return
-	//}
 }
 
 func (th *TelegramHandler) register() {
@@ -61,33 +64,20 @@ func (th *TelegramHandler) register() {
 	}
 }
 
-func (th *TelegramHandler) HelloAnswer() {
-	msg := telegram.NewMessage(th.update.Message.Chat.ID, "")
-	msg.ReplyToMessageID = th.update.Message.MessageID
-
-	msg.Text = "Здравствуйте, буду рад вам помочь!"
-	_, err := th.bot.Send(msg)
-	if err != nil {
-		return
-	}
-}
-
 func (th *TelegramHandler) DefaultAnswer() {
 	msg := telegram.NewMessage(th.update.Message.Chat.ID, "")
 	msg.ReplyToMessageID = th.update.Message.MessageID
 
-	var defaultKeyboard = telegram.NewInlineKeyboardMarkup(
-		telegram.NewInlineKeyboardRow(
-			//telegram.NewInlineKeyboardButtonURL("1.com", "http://1.com"),
-			//telegram.NewInlineKeyboardButtonData("2", "2"),
-			//telegram.NewInlineKeyboardButtonData("3", "3"),
-			telegram.InlineKeyboardButton{Text: "Создать быструю заметку"},
+	var defaultKeyboard = telegram.NewReplyKeyboard(
+		telegram.NewKeyboardButtonRow(
+			telegram.NewKeyboardButton("Быстрая заметка"),
+			telegram.NewKeyboardButton("Заметка"),
+			telegram.NewKeyboardButton("Напоминание"),
 		),
-		//telegram.NewInlineKeyboardRow(
-		//	telegram.NewInlineKeyboardButtonData("4", "4"),
-		//	telegram.NewInlineKeyboardButtonData("5", "5"),
-		//	telegram.NewInlineKeyboardButtonData("6", "6"),
-		//),
+		telegram.NewKeyboardButtonRow(
+			telegram.NewKeyboardButton("Помощь"),
+			telegram.NewKeyboardButton("Разработчику на кофе"),
+		),
 	)
 
 	msg.ReplyMarkup = defaultKeyboard
@@ -95,17 +85,7 @@ func (th *TelegramHandler) DefaultAnswer() {
 	msg.Text = "Что нужно сделать?"
 	_, err := th.bot.Send(msg)
 	if err != nil {
-		return
-	}
-}
-
-func (th *TelegramHandler) HelpAnswer() {
-	msg := telegram.NewMessage(th.update.Message.Chat.ID, "")
-	msg.ReplyToMessageID = th.update.Message.MessageID
-
-	msg.Text = "Чем могу вам помочь?"
-	_, err := th.bot.Send(msg)
-	if err != nil {
+		th.log.Error(err)
 		return
 	}
 }
