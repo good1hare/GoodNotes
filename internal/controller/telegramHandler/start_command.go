33836079de43b57c5ac960f5c686dcb97bbd7d64
Bulat Sharifullin <good1hare@gmail.com>
2023-08-03 16:55:03 +0300

    add sticker for hello command

--- internal/controller/telegramHandler/start_command.go
+++ internal/controller/telegramHandler/start_command.go
@@ -3,11 +3,15 @@ package telegramHandler
 import telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 func (th *TelegramHandler) StartCommand() {
-	msg := telegram.NewMessage(th.update.Message.Chat.ID, "")
-	msg.ReplyToMessageID = th.update.Message.MessageID
+	sticker := telegram.NewSticker(th.update.Message.Chat.ID, telegram.FileID("CAACAgIAAxkBAAKoG2TLsNnQLUCTwzMAAZn4KcKt7j3ftwACAQEAAladvQoivp8OuMLmNC8E"))
+	_, err := th.bot.Send(sticker)
+	if err != nil {
+		th.log.Error(err)
+	}
 
+	msg := telegram.NewMessage(th.update.Message.Chat.ID, "")
 	msg.Text = "Здравствуйте, буду рад вам помочь!"
-	_, err := th.bot.Send(msg)
+	_, err = th.bot.Send(msg)
 	if err != nil {
 		th.log.Error(err)
 	}
